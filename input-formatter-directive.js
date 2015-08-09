e171eb443788ba13468c0c1372f225c77b64dfbd
Ashwini Kumar <ashwini.kumar@hotelsoft.com>
2015-08-09 20:29:02 +0530

    added a custom directive for for formatting input fields with number / currency filters

---
+++ input-formatter-directive.js
@@ -0,0 +1,104 @@
+/**
+ * Created by ashwinikumar on 23/07/15.
+ * The directive purpose is to support the scenarios where we need to have different containment in the input-tag and the value stored against the model.
+ * Usage:
+ * 		Dependency Registration
+ * 			var module = ng.module('your-module-name', ['form.input.formatter']);
+ * 		In HTML-Template (Either of the following)
+ * 			<input input-formatter="number" type="text" ng-model="your-model"/>
+ * 			<input input-formatter="number:3" type="text" ng-model="your-model"/>
+ * 			<input input-formatter="currency" type="text" ng-model="your-model"/>
+ * 			<input input-formatter="currency:£" type="text" ng-model="your-model"/>
+ * 			<input input-formatter="currency:$:2" type="text" ng-model="your-model"/>
+ *
+ * 	Existing Minor Issues
+ * 		The `,` can not be deleted manually inside the input box -- Which is fine perhaps
+ * 		Currently only `number` and `currency` filters are supported.
+ *
+ */
+
+! function (ng) {
+	'use strict';
+
+	var customRender = function (el, prevVal, newVal, ngModelCtrl, setCursor) {
+
+		var start = el.selectionStart;
+		var end = el.selectionEnd + newVal.length - prevVal.length;
+
+		ngModelCtrl.$setViewValue(newVal);
+		ngModelCtrl.$render();
+
+		//if (setCursor)
+		el.setSelectionRange(end, end);
+	};
+
+	ng.module('form.input.formatter', []).directive('inputFormatter', ['$filter', function ($filter) {
+
+		return {
+			require: 'ngModel',
+			restrict: 'A',
+			link: function ($scope, $iElement, $iAttrs, ngModelCtrl) {
+
+				var inputFormatterAttrs = $iAttrs.inputFormatter.split(':'),
+					filterName, regEx,
+					el = $iElement[0],
+					firstParam, secondParam, precisionParam;
+
+				if (inputFormatterAttrs.length === 0) {
+					return;
+				}
+
+				filterName = inputFormatterAttrs[0];
+				firstParam = inputFormatterAttrs[1];
+				secondParam = inputFormatterAttrs[2];
+
+				if (filterName === 'number') {
+					precisionParam = parseInt(firstParam);
+				} else if (filterName === 'currency') {
+					precisionParam = parseInt(secondParam);
+				}
+
+				if (precisionParam === precisionParam && precisionParam !== 0) {
+					//precision param provided is a good integer and it is not zero
+					//That case we should scrap all non-digit integers and we should not scrap the last decimal point
+					regEx = /[^0-9.]|\.(?=.*\.)/g;
+				} else {
+					//precision param provided implied is zero
+					//That case we should scrap all non-digit integers (including dots/decimals)
+					regEx = /[^0-9]/g;
+				}
+
+				ngModelCtrl.$parsers.push(function toModel(viewValue) {
+
+					var modelValue = $filter(inputFormatterAttrs[0])(viewValue.toString().replace(regEx, ''), firstParam, secondParam);
+
+					if (modelValue === viewValue) {
+						return modelValue;
+					}
+
+					//customRender(el, viewValue, modelValue, ngModelCtrl, true);
+					customRender(el, viewValue, modelValue, ngModelCtrl);
+
+					modelValue = modelValue.replace(regEx, '');
+
+					return modelValue;
+				});
+
+				ngModelCtrl.$formatters.push(function toView(modelValue) {
+
+					//Taking care of the cases where the modelvalue from the database is not a string like null or some number value
+					modelValue = modelValue ? modelValue.toString() : '';
+
+					var viewValue = $filter(inputFormatterAttrs[0])(modelValue.replace(regEx, ''), firstParam, secondParam);
+
+					//customRender(el, viewValue, modelValue, ngModelCtrl);
+
+					return viewValue;
+				});
+
+			}
+		}
+
+	}]);
+
+}(angular);
